bb95eaff98630c1d2e4c229d350bd874c5bf1e48
Thirassia <78689650+Thirassia@users.noreply.github.com>
2021-11-21 15:51:08 +0200

    Add tests for compact.js. 4/5 tests fail.

---
+++ test/compact.test.js
@@ -0,0 +1,28 @@
+import chai from "chai";
+import compact from "../src/compact.js";
+
+const expect = chai.expect;
+
+// Testing compact function with different inputs
+describe("compact", () => {
+    it("returns a list with null value removed", () => {
+        const result = [1, 2];
+        expect(compact([null, 1, null, 2])).to.eql(result);
+    });
+    it("returns a list with 0 value removed", () => {
+        const result = [1, 2, 3];
+        expect(compact([1, 2, 0, 3])).to.eql(result);
+    });
+    it("returns a list with all falsey values removed", () => {
+        const result = [1, 2, 3];
+        expect(compact([1, false, 2, 0, "", 3, NaN])).to.eql(result);
+    });
+    it("returns the original list", () => {
+        const result = [1, 2, 3, 4, 5];
+        expect(compact([1, 2, 3, 4, 5])).to.eql(result);
+    });
+    it("returns an empty list with all falsey values removed", () => {
+        const result = [];
+        expect(compact([false, null, 0, "", undefined, NaN])).to.eql(result);
+    });
+});
