7da25bb796d28b6a1aaca683756d77e738b94275
Markus Uusitalo <markus.uusitalo@hotmail.fi>
2021-12-03 16:36:48 +0200

    refactored ceil.test.js

--- test/ceil.test.js
+++ test/ceil.test.js
@@ -1,53 +1,31 @@
-import chai from "chai";
 import ceil from "../src/ceil.js";
+import TestMaker from "./utility/utility.js";
 
-const expect = chai.expect;
+const testMaker = new TestMaker(ceil);
 
 // Testing ceil function with different inputs
 describe("ceil", () => {
-    it("rounds a positive number up according to precision", () => {
-        const result = 6.34;
-        expect(ceil(6.333,2)).to.equal(result);
-    });
-    it("rounds a negative number up according to precision", () => {
-        const result = -9.4;
-        expect(ceil(-9.44,1)).to.equal(result);
-    });
-    it("adds decimals to a positive number according to precision", () => {
-        const result = 5454.50;
-        expect(ceil(5454.5,2)).to.equal(result);
-    });
-    it("adds decimals to a negative number according to precision", () => {
-        const result = -24.5000;
-        expect(ceil(-24.5,4)).to.equal(result);
-    });
-    it("rounds a positive number up with negative precision", () => {
-        const result = 2200;
-        expect(ceil(2125,-2)).to.equal(result);
-    });
-    it("rounds a negative number up with negative precision", () => {
-        const result = -320;
-        expect(ceil(-325,-1)).to.equal(result);
-    });
-    it("rounds a positive number up with negative precision greater than the positive number", () => {
-        const result = 10000;
-        expect(ceil(5.3,-4)).to.equal(result);
-    });
-    it("returns zero with negative precision greater than the negative number", () => {
-        const result = 0;
-        expect(ceil(-5.2,-4)).to.equal(result);
-    });
-    it("cannot round a positive number up with decimal precision", () => {
-        // cannot use decimals as precision, false premise in original test
-        const result = NaN;
-        expect(ceil(333.33,1.11)).to.eql(result);
-    });
-    it("rounds a positive integer up with positive precision", () => {
-        const result = 325.0;
-        expect(ceil(325,1)).to.equal(result);
-    });
-    it("rounds a positive number up to an integer without precision", () => {
-        const result = 1000;
-        expect(ceil(999.99)).to.equal(result);
-    });
+    testMaker.testCase("Round up a positive number according to specified positive precision. "
+        + "Number is of greater precision than specified precision", 6.34, [6.333, 2]);
+    testMaker.testCase("Round up a negative number according to specified positive precision. "
+        + "Number is of greater precision than specified precision", -9.4, [-9.433, 1]);
+    testMaker.testCase("Add decimals to a positive number according to specified positive precision. "
+        + "Float makes the result the same as the input", 5454.5, [5454.50, 2]);
+    testMaker.testCase("Add decimals to a negative number according to specified positive precision. "
+        + "Float makes the result the same as the input", -24.5, [-24.5000, 4]);
+    testMaker.testCase("Round up a positive number according to specified negative precision. "
+        + "Number is of greater precision than specified precision", 2200, [2125, -2]);
+    testMaker.testCase("Round up a negative number according to specified negative precision. "
+        + "Number is of greater precision than specified precision", -320, [-325, -1]);
+    testMaker.testCase("Round up a positive number according to specified precision. "
+        + "Number is of lesser precision than specified precision", 10000, [5.3, -4]);
+    testMaker.testCase("Return 0 with a negative number of lesser precision "
+        + "than specified negative precision.", 0, [-5.2, -4]);
+
+    // Cannot compute with a decimal precision value
+    testMaker.testCase("Return NaN when trying to round up a positive number "
+        + "according to specified decimal precision", NaN, [333.33, 4.11]);
+    testMaker.testCase("Round up a positive integer according to specified positive precision. "
+        + "Float makes the result the same as the input", 325.0, [325, 1]);
+    testMaker.testCase("Round a positive number according to default precision", 1000, [999.99]);
 });
