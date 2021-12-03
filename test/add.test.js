9a3223def3dce62304811ea8f0237875d1ca3d9f
Markus Uusitalo <markus.uusitalo@hotmail.fi>
2021-12-03 16:35:43 +0200

    refactored add.test.js

--- test/add.test.js
+++ test/add.test.js
@@ -1,28 +1,14 @@
-import chai from "chai";
 import add from "../src/add.js";
+import TestMaker from "./utility/utility.js";
 
-const expect = chai.expect;
+const testMaker = new TestMaker(add);
 
 // Testing add function with different inputs
 describe("add", () => {
-    it("adds two positive decimal numbers together", () => {
-        const result = 16.8;
-        expect(add(6.9,9.9)).to.eql(result);
-    });
-    it("adds a positive decimal number and 0 together", () => {
-        const result = 699.999;
-        expect(add(699.999,0)).to.eql(result);
-    });
-    it("adds a positive decimal number and a negative decimal number together", () => {
-        const result = 5354.51;
-        expect(add(5454.5,-99.99)).to.eql(result);
-    });
-    it("adds two negative decimal numbers together", () => {
-        const result = -84.4;
-        expect(add(-24.5,-59.9)).to.eql(result);
-    });
-    it("adds a negative decimal number and 0 together", () => {
-        const result = -20.25;
-        expect(add(-20.25,0)).to.eql(result);
-    });
+    testMaker.testCase("adds two positive decimal numbers together", 16.8, [9.9, 6.9]);
+    testMaker.testCase("adds a positive decimal number and 0 together", 699.999, [0, 699.999]);
+    testMaker.testCase("adds a positive decimal number and a negative decimal number together", 5354.51, [-99.99, 5454.5]);
+    testMaker.testCase("adds two negative decimal numbers together", -84.4, [-59.9, -24.5]);
+    testMaker.testCase("adds a negative decimal number and 0 together", -20.25, [0, -20.25]);
+    testMaker.testCase("adds 0 and 0 together", 0, [0, 0]);
 });
