acd7d9da98ecc7d7da553b7b1e4a293302cded85
Markus Uusitalo <markus.uusitalo@hotmail.fi>
2021-12-03 16:40:55 +0200

    toNumber.test.js

--- test/toNumber.test.js
+++ test/toNumber.test.js
@@ -1,77 +1,35 @@
-import chai from "chai";
 import toNumber from "../src/toNumber.js";
+import TestMaker from "./utility/utility.js";
 
-const expect = chai.expect;
+const testMaker = new TestMaker(toNumber);
+
+// eslint-disable-next-line no-unused-vars
+function testFunction() {
+
+}
 
 // Testing toNumber function with different inputs
 describe("toNumber", () => {
-    it("returns 0 as is", () => {
-        const result = 0;
-        expect(toNumber(0)).to.eql(result);
-    });
-    it("returns positive integer as is", () => {
-        const result = 699;
-        expect(toNumber(699)).to.eql(result);
-    });
-    it("returns negative integer as is", () => {
-        const result = -5454;
-        expect(toNumber(-5454)).to.eql(result);
-    });
-    it("returns positive decimal number as is", () => {
-        const result = 84.45;
-        expect(toNumber(84.45)).to.eql(result);
-    });
-    it("returns negative decimal number as is", () => {
-        const result = -20.25;
-        expect(toNumber(-20.25)).to.eql(result);
-    });
-    it("returns text as positive integer", () => {
-        const result = 20;
-        expect(toNumber("20")).to.eql(result);
-    });
-    it("returns text as negative integer", () => {
-        const result = -2078;
-        expect(toNumber("-2078")).to.eql(result);
-    });
-    it("returns text as positive decimal number", () => {
-        const result = 78.4;
-        expect(toNumber("78.4")).to.eql(result);
-    });
-    it("returns text as negative decimal number", () => {
-        const result = -7.4;
-        expect(toNumber("-7.4")).to.eql(result);
-    });
-    it("returns text resembling binary number as number", () => {
-        const result = 1111100111;
-        expect(toNumber("1111100111")).to.eql(result);
-    });
-    it("returns text resembling octal number as number", () => {
-        const result = 123;
-        expect(toNumber("123")).to.eql(result);
-    });
-    it("returns text resembling hexadecimal number as number", () => {
-        const result = 5E-64;
-        expect(toNumber("5E-64")).to.eql(result);
-    });
-    it("returns text with whitespaces as decimal number", () => {
-        const result = 7.4;
-        expect(toNumber("  7.4")).to.eql(result);
-    });
-    it("returns text resembling binary decimal number as number", () => {
-        const result = 0.1011;
-        expect(toNumber("0.1011")).to.eql(result);
-    });
-    it("returns text as NaN", () => {
-        const result = NaN;
-        expect(toNumber("text")).to.eql(result);
-    });
-    it("returns number with multiple dots as NaN", () => {
-        const result = NaN;
-        expect(toNumber("34.2.6")).to.eql(result);
-    });
-    // new test to increase coverage
-    it("returns object as NaN", () => {
-        const result = NaN;
-        expect(toNumber({})).to.eql(result);
-    });
+    testMaker.testCase("Return 0 as is.", 0, [0]);
+    testMaker.testCase("Return positive integer as is", 699, [699]);
+    testMaker.testCase("Return negative integer as is.", -5454, [-5454]);
+    testMaker.testCase("Return positive decimal number as is", 84.45, [84.45]);
+    testMaker.testCase("Return negative decimal number as is", -20.25, [-20.25]);
+    testMaker.testCase("Return text as positive integer", 20, ["20"]);
+    testMaker.testCase("Return text as negative integer", -2078, ["-2078"]);
+    testMaker.testCase("Return text as positive decimal number", 78.4, ["78.4"]);
+    testMaker.testCase("Return text as negative decimal number", -7.4, ["-7.4"]);
+    testMaker.testCase("Return text representing binary number as number", 0b1111, ["0b1111"]);
+    testMaker.testCase("Return text representing octal number as number", 0o17, ["0o17"]);
+    testMaker.testCase("Return text representing hexadecimal number as number", 0xF, ["0xF"]);
+    testMaker.testCase("Return text starting with whitespace as decimal number", 7.4, ["  7.4"]);
+    testMaker.testCase("Return text reprenting a binary number fraction as NaN", NaN, ["0b11.111"]);
+    testMaker.testCase("Return text not representing a number as NaN", NaN, ["text"]);
+    testMaker.testCase("Return text representing a number with multiple dots as NaN", NaN, ["34.2.6"]);
+    testMaker.testCase("Return object as NaN", NaN, [{ 23: 43 }]);
+    testMaker.testCase("Return symbol as NaN", NaN, [Symbol(12)]);
+    testMaker.testCase("Return null as 0", 0, [null]);
+    testMaker.testCase("Return function as NaN", NaN, [testFunction]);
+    testMaker.testCase("Return Infinity as Infinity", Infinity, [Infinity]);
+    testMaker.testCase("Return text representing Infinity as Infinity", Infinity, ["Infinity"]);
 });
